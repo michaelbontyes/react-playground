01df68a76dc87fc45ca2eca85ff66dfe775b4214
Michou <michaelbontyes@gmail.com>
2016-02-13 17:49:27 -0500

    refs

--- app/components/App.js
+++ app/components/App.js
@@ -4,24 +4,51 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {txt: ''}
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    }
     this.update = this.update.bind(this)
   }
   update(e){
-    this.setState({txt: e.target.value})
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    })
   }
   render() {
     return(
       <div>
-      <input type="text"
-        onChange={this.update.bind(this)} />
-        <Widget txt={this.state.txt} />
-        <Widget txt={this.state.txt} />
-        <Widget txt={this.state.txt} />
-        </div>
+
+        <Slider ref="red" update={this.update}/>
+        {this.state.red}
+        <br />
+        <Slider ref="green" update={this.update}/>
+        {this.state.green}
+        <br />
+        <Slider ref="blue" update={this.update}/>
+        {this.state.blue}
+        <br />
+      </div>
     );
   }
 }
+
+class Slider extends React.Component {
+  render() {
+    return(
+      <div>
+      <input ref="inp" type="range"
+        min="0"
+        max="255"
+        onChange={this.props.update} />
+      </div>
+    )
+  }
+}
+
 
 const Widget = (props) => {
   return(
