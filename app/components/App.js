35599cc20fb1efcee3e57618176cd0764af0f8f6
Michou <michaelbontyes@gmail.com>
2016-02-13 18:14:17 -0500

    children

--- app/components/App.js
+++ app/components/App.js
@@ -1,71 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    }
-    this.update = this.update.bind(this)
+class App extends React.Component{
+  render(){
+    return <Button>I <Heart/> React</Button>
   }
-  update(e){
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-    })
-  }
+}
+
+class Button extends React.Component {
   render() {
-    return(
-      <div>
-
-        <Slider ref="red" update={this.update}/>
-        {this.state.red}
-        <br />
-        <Slider ref="green" update={this.update}/>
-        {this.state.green}
-        <br />
-        <Slider ref="blue" update={this.update}/>
-        {this.state.blue}
-        <br />
-      </div>
-    );
+    return <button className="uk-button">{this.props.children}</button>
   }
 }
 
-class Slider extends React.Component {
-  render() {
-    return(
-      <div>
-      <input ref="inp" type="range"
-        min="0"
-        max="255"
-        onChange={this.props.update} />
-      </div>
-    )
-  }
-}
-
-
-const Widget = (props) => {
-  return(
-    <div>
-      <h1>{props.txt}</h1>
-    </div>
-  );
-}
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  //cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps ={
-  txt: 'this is the default txt yo'
-}
+const Heart = () => <i className="uk-icon-heart"></i>
 
 ReactDOM.render(
   <App/>,
